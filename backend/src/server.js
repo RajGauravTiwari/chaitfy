d63626edcccecb9dcf78750c4e6def4fe2b955e7
RajGauravTiwari <raj.tiwari@iitg.ac.in>
2026-01-18 11:05:38 +0530

    update profile endpoints added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import path from "path"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
@@ -13,6 +15,7 @@ const PORT = (process.env.PORT) || 3000;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/message",messageRoutes);
 
